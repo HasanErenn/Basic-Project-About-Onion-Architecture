0ca15e4008552493c62365b2dfff48521e5a7890
Hasan Eren -Seza <Hasan Eren -Seza@DESKTOP-N1R96ME>
2023-01-03 13:08:00 +0300

    Add project files.

---
+++ Infrastructure/Persistance/Concretes/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using EticaretAPI.Domain.Entities;
+using ETicaretAPI.Application.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Products> GetProducts() =>
+            new() //Target Type Ozelligi
+            {
+                new() {id=Guid.NewGuid(), Name="Product 1", Stock=10,Price=22 },
+                new() {id=Guid.NewGuid(), Name="Product 5", Stock=9,Price=65 },
+                new() {id=Guid.NewGuid(), Name="Product 6", Stock=0,Price=67 },
+                new() {id=Guid.NewGuid(), Name="Product 7", Stock=23,Price=32 },
+            };
+    }
+}
---
+++ Infrastructure/Persistance/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services /*IoC Container'a ekleme islemini yapiyoruz*/ ) //bu bir extension method'dur.
+        {
+            services.AddSingleton<IProductService, ProductService>();
+            //IProductService turunden bir istek geldiginde ProductService'i gonder.
+
+            //Bu extension method IoC Container'i barindiran Presentation'daki ASP.NET Core uygulamasi tarafindan cagirilmasi gerekiyor. Program.cs'de cagiriyoruz.
+        }
+    }
+}
